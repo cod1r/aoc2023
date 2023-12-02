b440f5be085d3153a1c541d1a918d635ded566ec
cod1r <jasonho353@gmail.com>
2023-12-02 14:34:19 -0600

    no more heap allocation for day2

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -8,12 +8,12 @@ int32_t max(int32_t a, int32_t b) {
   return a < b ? b:a;
 }
 // returned results will always be rgb
-std::array<int32_t, 3> parse_set(const std::string& set) {
+std::array<int32_t, 3> parse_set(char *set, int32_t length) {
   int32_t red = 0;
   int32_t green = 0;
   int32_t blue = 0;
   int32_t start_idx = 0;
-  for (int32_t idx = 0; idx < (int32_t)set.length(); ++idx) {
+  for (int32_t idx = 0; idx < length; ++idx) {
     if (set[idx] == ' ' && set[idx + 1] >= 97 && set[idx + 1] <= 123) {
       int32_t val = 0;
       for (int32_t digit_idx = start_idx; digit_idx < idx; ++digit_idx) {
@@ -62,34 +62,28 @@ int32_t main() {
     std::vector<std::string> sets;
     int32_t idx_of_start_of_sets = idx_of_colon;
     int32_t prev_idx_of_semi = idx_of_start_of_sets;
-    for (int32_t idx = idx_of_start_of_sets; idx < line_length; ++idx) {
-      if (line[idx] == ';') {
-        sets.push_back(
-            // adding 2 because of semi and space
-            std::string(line + prev_idx_of_semi + 2, idx - (prev_idx_of_semi + 2))
-        );
-        prev_idx_of_semi = idx;
-      }
-    }
-    // adding 2 because of semi and space
-    sets.push_back(std::string(line + prev_idx_of_semi + 2, line_length - (prev_idx_of_semi + 2)));
-    sum_of_game_ids += game_id;
-    for (std::string& set : sets) {
-      std::array<int32_t, 3> rgb = parse_set(set);
-      if (rgb[0] > 12 || rgb[1] > 13 || rgb[2] > 14) {
-        sum_of_game_ids -= game_id;
-        break;
-      }
-    }
+    bool add = true;
     int32_t max_red = 0;
     int32_t max_green = 0;
     int32_t max_blue = 0;
-    for (std::string& set : sets) {
-      std::array<int32_t, 3> rgb = parse_set(set);
-      max_red = max(rgb[0], max_red);
-      max_green = max(rgb[1], max_green);
-      max_blue = max(rgb[2], max_blue);
+    for (int32_t idx = idx_of_start_of_sets; idx < line_length; ++idx) {
+      if (line[idx] == ';' || idx == line_length - 1) {
+        if (idx == line_length - 1) {
+          idx = line_length;
+        }
+        // adding 2 because of semi and space
+        std::array<int32_t, 3> rgb = parse_set(line + prev_idx_of_semi + 2, idx - (prev_idx_of_semi + 2));
+        max_red = max(rgb[0], max_red);
+        max_green = max(rgb[1], max_green);
+        max_blue = max(rgb[2], max_blue);
+        if (rgb[0] > 12 || rgb[1] > 13 || rgb[2] > 14) {
+          add = false;
+        }
+        prev_idx_of_semi = idx;
+      }
     }
+    if (add)
+      sum_of_game_ids += game_id;
     sum_of_rgb_multiplied += max_red * max_green * max_blue;
   }
   input.close();
