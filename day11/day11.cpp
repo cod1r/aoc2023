42bc491d044d2b3d6f006839651765180a26410b
cod1r <jasonho353@gmail.com>
2023-12-11 19:44:26 -0600

    never mind about the last commit msg; Going from a smaller type to a bigger type is always safe (the bits were zeroed out so there shouldn't be any case where casting a smaller type to a bigger type gives you some random none-zero bits); The actual issue was that the initial type was int32_t, which meant that the accumulator in the lambda also had type int32_t even though the accumulator type was specified in the lambda as int64_t. I'm guessing the compiler just uses the initial type and just checks whether that initial type can be converted to the type in the lambda

--- day11/day11.cpp
+++ day11/day11.cpp
@@ -90,7 +90,7 @@ const std::vector<int32_t>& cols
 ) {
   std::vector<int64_t> row_seq = generate_seq(times, (int32_t)image.size(), rows);
   std::vector<int64_t> col_seq = generate_seq(times, (int32_t)image[0].length(), cols);
-  int64_t sum = std::accumulate(pairs.begin(), pairs.end(), (int64_t)0, [&row_seq, &col_seq](int64_t acc, const auto& p) {
+  int64_t sum = std::accumulate(pairs.begin(), pairs.end(), (int64_t)0, [&row_seq, &col_seq](auto acc, const auto& p) {
     int64_t converted_row_p0 = (int64_t)row_seq[p[0].first];
     int64_t converted_row_p1 = (int64_t)row_seq[p[1].first];
     int64_t converted_col_p0 = (int64_t)col_seq[p[0].second];
