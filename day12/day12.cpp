60f73f2baebdcb4d33d60a522e40a4842aca77d9
cod1r <jasonho353@gmail.com>
2023-12-17 23:57:56 -0600

    FINALLY GOT day12 part2, top down dfs/backtracking with memoization

--- day12/day12.cpp
+++ day12/day12.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <algorithm>
 #include <array>
-#include <fstream>
-#include <tuple>
-#include <optional>
+#include <cassert>
 #include <cmath>
 #include <cstdint>
-#include <algorithm>
-#include <utility>
+#include <format>
+#include <fstream>
+#include <iostream>
 #include <map>
-#include <set>
 #include <numeric>
-#include <cassert>
+#include <optional>
 #include <ranges>
+#include <set>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
 
 int64_t parse_number(std::string_view s) {
   int32_t length = (int32_t)s.length();
@@ -23,7 +24,8 @@ int64_t parse_number(std::string_view s) {
   }
   return value;
 }
-std::vector<int64_t> parse_string_to_vec_numbers(char *line, size_t length, char delimiter = ' ') {
+std::vector<int64_t> parse_string_to_vec_numbers(char *line, size_t length,
+                                                 char delimiter = ' ') {
   std::vector<int64_t> numbers;
   enum class Sign {
     Pos,
@@ -32,78 +34,101 @@ std::vector<int64_t> parse_string_to_vec_numbers(char *line, size_t length, char
   std::vector<std::pair<Sign, std::string_view>> num_strings;
   std::optional<int32_t> digit_start;
   for (size_t num_parser_idx = 0; num_parser_idx < length; ++num_parser_idx) {
-    if (
-      ((line[num_parser_idx] >= '0' && line[num_parser_idx] <= '9') || line[num_parser_idx] == '-') &&
-      !digit_start.has_value()) {
+    if (((line[num_parser_idx] >= '0' && line[num_parser_idx] <= '9') ||
+         line[num_parser_idx] == '-') &&
+        !digit_start.has_value()) {
       digit_start = num_parser_idx;
     }
-    if ((line[num_parser_idx] == delimiter || num_parser_idx == length - 1) && digit_start.has_value()) {
+    if ((line[num_parser_idx] == delimiter || num_parser_idx == length - 1) &&
+        digit_start.has_value()) {
       if (num_parser_idx == length - 1)
         num_parser_idx = length;
       bool has_neg = line[*digit_start] == '-';
       int32_t actual_digit_start = has_neg ? *digit_start + 1 : *digit_start;
       num_strings.push_back(
-      {has_neg ? Sign::Neg : Sign::Pos,
-      std::string_view(line + actual_digit_start, num_parser_idx - actual_digit_start)});
+          {has_neg ? Sign::Neg : Sign::Pos,
+           std::string_view(line + actual_digit_start,
+                            num_parser_idx - actual_digit_start)});
       digit_start.reset();
     }
   }
   for (auto [sign, seed_str] : num_strings) {
     auto num = parse_number(seed_str);
     switch (sign) {
-      case Sign::Pos: numbers.push_back(num); break;
-      case Sign::Neg: numbers.push_back(-num); break;
+    case Sign::Pos:
+      numbers.push_back(num);
+      break;
+    case Sign::Neg:
+      numbers.push_back(-num);
+      break;
     }
   }
   return numbers;
 }
-template<typename T> requires std::is_integral_v<T>
-std::ostream& operator<<(std::ostream &os, const std::vector<T> &v) {
+template <typename T>
+  requires std::is_integral_v<T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
   for (const T &n : v) {
     os << n << " ";
   }
   os << std::endl;
   return os;
 }
-void dfs_concat_groups(const std::vector<std::vector<std::string>>& variants_of_groups,
-size_t length_of_springs, const std::string& filter_str,
-std::string curr_concat, size_t idx_group, std::vector<std::string>& out) {
+void dfs_concat_groups(
+    const std::vector<std::vector<std::string>> &variants_of_groups,
+    size_t length_of_springs, const std::string &filter_str,
+    std::string curr_concat, size_t idx_group, std::vector<std::string> &out) {
   for (size_t idx_filter_str = 0; idx_filter_str < filter_str.length() &&
-    idx_filter_str < curr_concat.length(); ++idx_filter_str) {
-    if ((filter_str[idx_filter_str] == '.' || filter_str[idx_filter_str] == '#') &&
-      curr_concat[idx_filter_str] != filter_str[idx_filter_str])
+                                  idx_filter_str < curr_concat.length();
+       ++idx_filter_str) {
+    if ((filter_str[idx_filter_str] == '.' ||
+         filter_str[idx_filter_str] == '#') &&
+        curr_concat[idx_filter_str] != filter_str[idx_filter_str])
       return;
   }
-  if (curr_concat.length() == length_of_springs && idx_group == variants_of_groups.size()) {
+  if (curr_concat.length() == length_of_springs &&
+      idx_group == variants_of_groups.size()) {
     out.push_back(curr_concat);
     return;
   }
   if (idx_group == variants_of_groups.size())
     return;
-  for (size_t idx_variant_of_group = 0; idx_variant_of_group < variants_of_groups[idx_group].size();
-    ++idx_variant_of_group) {
-    const std::string& variant_group = variants_of_groups[idx_group][idx_variant_of_group];
+  for (size_t idx_variant_of_group = 0;
+       idx_variant_of_group < variants_of_groups[idx_group].size();
+       ++idx_variant_of_group) {
+    const std::string &variant_group =
+        variants_of_groups[idx_group][idx_variant_of_group];
     if (curr_concat.length() + variant_group.length() <= length_of_springs) {
-      dfs_concat_groups(
-          variants_of_groups,
-          length_of_springs,
-          filter_str,
-          curr_concat.length() > 0 ? curr_concat + "." + variant_group:variant_group,
-          idx_group + 1,
-          out
-      );
+      dfs_concat_groups(variants_of_groups, length_of_springs, filter_str,
+                        curr_concat.length() > 0
+                            ? curr_concat + "." + variant_group
+                            : variant_group,
+                        idx_group + 1, out);
     }
   }
 }
+std::vector<std::string>
+filter_down_variant_group(const std::vector<std::string> &variant_group,
+                          const std::string &record, size_t idx_start) {
+  auto filtered = std::views::filter(
+      variant_group, [&idx_start, record](const std::string &group) {
+        for (size_t start = idx_start; start < idx_start + group.length();
+             ++start) {
+          if ((record[start] == '.' || record[start] == '#') &&
+              group[start - idx_start] != record[start])
+            return false;
+        }
+        return true;
+      });
+  return std::vector<std::string>(filtered.begin(), filtered.end());
+}
 std::vector<std::string> generate_arrangements_based_off_of_group_size(
-    const std::vector<int64_t>& group_size,
-    const std::string& filter_str,
-    size_t length_of_springs
-    ) {
+    const std::vector<int64_t> &group_size, const std::string &filter_str,
+    size_t length_of_springs) {
   std::vector<std::string> arrangements;
   std::vector<std::string> groups;
   size_t summed_up_sizes = 0;
-  for (const int64_t& size : group_size) {
+  for (const int64_t &size : group_size) {
     summed_up_sizes += (size_t)size;
     groups.push_back(std::string(size, '#'));
   }
@@ -114,12 +139,14 @@ std::vector<std::string> generate_arrangements_based_off_of_group_size(
   size_t left_over = length_of_springs - (groups.size() - 1 + summed_up_sizes);
   for (size_t idx_groups = 0; idx_groups < groups.size(); ++idx_groups) {
     std::string cpy_group = groups[idx_groups];
-    size_t summed_up_sizes_except_for_this_group = summed_up_sizes - cpy_group.length();
+    size_t summed_up_sizes_except_for_this_group =
+        summed_up_sizes - cpy_group.length();
     for (size_t left_amt = 0; left_amt <= left_over; ++left_amt) {
-      for (size_t right_amt = 0; right_amt <= left_over - left_amt; ++right_amt) {
-        if (left_amt +
-          right_amt +
-          cpy_group.length() + groups.size() - 1 + summed_up_sizes_except_for_this_group <= length_of_springs) {
+      for (size_t right_amt = 0; right_amt <= left_over - left_amt;
+           ++right_amt) {
+        if (left_amt + right_amt + cpy_group.length() + groups.size() - 1 +
+                summed_up_sizes_except_for_this_group <=
+            length_of_springs) {
           if (right_amt > 0) {
             std::string right(right_amt, '.');
             cpy_group.append(right);
@@ -134,14 +161,71 @@ std::vector<std::string> generate_arrangements_based_off_of_group_size(
       }
     }
   }
-  //auto _ = filter_str;
-  //for (const auto& v : variants_of_groups) {
-  //  for (const auto& s : v) {
-  //    std::cout << s << std::endl;
-  //  }
-  //}
-  dfs_concat_groups(variants_of_groups, length_of_springs, filter_str, std::string{}, 0, arrangements);
+  variants_of_groups[0] =
+      filter_down_variant_group(variants_of_groups[0], filter_str, 0);
+  dfs_concat_groups(variants_of_groups, length_of_springs, filter_str,
+                    std::string{}, 0, arrangements);
   return arrangements;
+}
+size_t dfs_with_group_size(const std::string &record, size_t idx_record,
+                           const std::vector<int64_t> &group_sizes,
+                           size_t idx_group_size,
+                           std::vector<std::vector<int64_t>> &memo) {
+  if (idx_group_size == group_sizes.size()) {
+    bool good_start = true;
+    for (size_t checking_for_dysfunctional_after_last = idx_record;
+         checking_for_dysfunctional_after_last < record.length();
+         ++checking_for_dysfunctional_after_last) {
+      if (record[checking_for_dysfunctional_after_last] == '#') {
+        good_start = false;
+        break;
+      }
+    }
+    if (!good_start) {
+      return 0;
+    }
+    return 1;
+  }
+  if (memo[idx_record][idx_group_size] != -1) {
+    return memo[idx_record][idx_group_size];
+  }
+  size_t current_size = group_sizes[idx_group_size];
+  size_t total_group_sizes = std::accumulate(
+      group_sizes.begin() + idx_group_size, group_sizes.end(), size_t{0});
+  size_t idx_end =
+      record.length() -
+      (total_group_sizes + ((group_sizes.size() - idx_group_size) - 1));
+  size_t for_sure_dysfunctional = idx_end;
+  for (size_t idx_find_for_sure_dysfunctional = idx_record;
+       idx_find_for_sure_dysfunctional <= idx_end;
+       ++idx_find_for_sure_dysfunctional) {
+    if (record[idx_find_for_sure_dysfunctional] == '#') {
+      for_sure_dysfunctional = idx_find_for_sure_dysfunctional;
+      break;
+    }
+  }
+  size_t return_val = 0;
+  for (size_t idx_start = idx_record; idx_start <= for_sure_dysfunctional;
+       ++idx_start) {
+    bool good_start = true;
+    for (size_t idx_check = idx_start; idx_check < idx_start + current_size;
+         ++idx_check) {
+      if (record[idx_check] == '.') {
+        good_start = false;
+        break;
+      }
+    }
+    if (idx_start + current_size < record.length() &&
+        record[idx_start + current_size] == '#') {
+      good_start = false;
+    }
+    if (good_start) {
+          return_val += dfs_with_group_size(record, idx_start + current_size + 1, group_sizes,
+                              idx_group_size + 1, memo);
+    }
+  }
+  memo[idx_record][idx_group_size] = return_val;
+  return return_val;
 }
 int32_t main(int32_t argc, char *argv[]) {
   if (argc != 2) {
@@ -162,65 +246,62 @@ int32_t main(int32_t argc, char *argv[]) {
     for (size_t idx_space = 0; idx_space < line_length; ++idx_space) {
       if (line[idx_space] == ' ') {
         condition_records.push_back(std::string(line, idx_space));
-        group_sizes.push_back(parse_string_to_vec_numbers(line + idx_space, line_length - idx_space, ','));
+        group_sizes.push_back(parse_string_to_vec_numbers(
+            line + idx_space, line_length - idx_space, ','));
         break;
       }
     }
   }
   int64_t part1 = 0;
-  for (size_t idx_condition_record = 0; idx_condition_record < condition_records.size();
-      ++idx_condition_record) {
-    std::vector<std::string> a =
-      generate_arrangements_based_off_of_group_size(group_sizes[idx_condition_record],
-      condition_records[idx_condition_record],
-      condition_records[idx_condition_record].length());
-    const std::string& record = condition_records[idx_condition_record];
-    auto filter = std::views::filter(a, [record](const std::string& arrangement) {
-      for (size_t idx_record = 0; idx_record < record.length(); ++idx_record) {
-        if ((record[idx_record] == '.' || record[idx_record] == '#') &&
-          arrangement[idx_record] != record[idx_record]) {
-          return false;
-        }
-      }
-      return true;
-    });
+  std::vector<size_t> right_ans(condition_records.size(), 0);
+  for (size_t idx_condition_record = 0;
+       idx_condition_record < condition_records.size();
+       ++idx_condition_record) {
+    std::vector<std::string> a = generate_arrangements_based_off_of_group_size(
+        group_sizes[idx_condition_record],
+        condition_records[idx_condition_record],
+        condition_records[idx_condition_record].length());
+    const std::string &record = condition_records[idx_condition_record];
+    auto filter =
+        std::views::filter(a, [record](const std::string &arrangement) {
+          for (size_t idx_record = 0; idx_record < record.length();
+               ++idx_record) {
+            if ((record[idx_record] == '.' || record[idx_record] == '#') &&
+                arrangement[idx_record] != record[idx_record]) {
+              return false;
+            }
+          }
+          return true;
+        });
     std::set<std::string> filtered(filter.begin(), filter.end());
+    right_ans[idx_condition_record] = filtered.size();
     part1 += filtered.size();
   }
   std::cout << "PART1: " << part1 << std::endl;
 
-  std::vector<std::string> part2_condition_records;
-  std::transform(condition_records.begin(), condition_records.end(), std::back_inserter(part2_condition_records),
-      [](const std::string& cr) {
-      return cr + "?" + cr + "?" + cr + "?" + cr + "?" + cr;
-      });
-  std::vector<std::vector<int64_t>> part2_group_sizes;
-  std::ranges::transform(group_sizes.begin(),
-      group_sizes.end(),
-      std::back_inserter(part2_group_sizes), [](const std::vector<int64_t>& gs) {
-      std::vector<int64_t> gs_cpy;
-      gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
-      gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
-      gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
-      gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
-      gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
-      return gs_cpy;
-      });
-  int64_t part2 = 0;
-  for (size_t idx_condition_record = 0; idx_condition_record < part2_condition_records.size();
-      ++idx_condition_record) {
-    std::string condition_record = part2_condition_records[idx_condition_record];
-    for (size_t idx_record = 0; idx_record < condition_record.length(); ++idx_record) {
-    }
+  std::transform(condition_records.begin(), condition_records.end(),
+                 condition_records.begin(), [](const std::string &cr) {
+                   return cr + "?" + cr + "?" + cr + "?" + cr + "?" + cr;
+                 });
+  std::transform(group_sizes.begin(), group_sizes.end(), group_sizes.begin(),
+                 [](const std::vector<int64_t> &gs) {
+                   std::vector<int64_t> gs_cpy;
+                   gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
+                   gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
+                   gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
+                   gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
+                   gs_cpy.insert(gs_cpy.begin(), gs.begin(), gs.end());
+                   return gs_cpy;
+                 });
+
+  size_t part2 = 0;
+  for (size_t idx = 0; idx < condition_records.size(); ++idx) {
+    const std::string &record = condition_records[idx];
+    std::vector<std::vector<int64_t>> memo(
+        record.length(), std::vector<int64_t>(group_sizes[idx].size(), -1));
+    part2 += dfs_with_group_size(record, 0, group_sizes[idx], 0, memo);
   }
   std::cout << "PART2: " << part2 << std::endl;
-    const std::string& test_str = "..#####.";
-    std::vector<std::string> a =
-      generate_arrangements_based_off_of_group_size({5},
-      test_str,
-      test_str.length());
-    for (const auto& s : std::set<std::string>(a.begin(), a.end())) {
-      std::cout << s << std::endl;
-    }
+
   return 0;
 }
